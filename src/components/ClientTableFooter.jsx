46f405bc1f1f7b1ddbb5e1d2aa40a07cca087bc3
medido <medi1222@gmail.com>
2026-01-05 13:57:13 +0100

    Add export data button UI

--- src/components/ClientTableFooter.jsx
+++ src/components/ClientTableFooter.jsx
@@ -1,6 +1,6 @@
 function ClientTableFooter({currentPage, totalPages, setCurrentPage}) {
   return (
-    <footer className="bg-gray-200 flex justify-center p-2">
+    <footer className="bg-gray-200 flex justify-center p-2 relative">
       <button 
         disabled={currentPage === 1}
         className="px-4 py-2 bg-blue-300 rounded hover:bg-blue-400 disabled:opacity-50
@@ -20,6 +20,14 @@ function ClientTableFooter({currentPage, totalPages, setCurrentPage}) {
       >
         Next
       </button>
+      <div className="absolute right-20 sm:right-10 bottom-2 flex items-center gap-4">
+        <button
+          className=" bg-blue-300 rounded px-4 py-2 hover:bg-blue-400
+          disabled:opacity-50 cursor-pointer"
+        >
+          Export 
+        </button>
+      </div>
     </footer>
   )
 }
