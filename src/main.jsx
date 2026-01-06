21b060d67fb011fae33079d6de39a740b65d49aa
medido <medi1222@gmail.com>
2026-01-06 13:15:19 +0100

    Enable offline support with PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,14 @@ import {UserProvider} from './context/userContext.jsx';
 import { InterfaceProvider } from './context/inferfaceContext.jsx';
 import { PrintProvider } from './context/PrintContext.jsx';
 import { DataProvider } from './context/DataContext.jsx';
-
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.jsx'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
